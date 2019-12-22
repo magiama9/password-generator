fea2648c6159008c06e593a9d6d2e4fb4a318cf6
magiama9 <58517943+magiama9@users.noreply.github.com>
2019-12-22 11:27:07 -0500

    Create /assets/ and initialize assets/scripts.js with pseudocode.

---
+++ assets/scripts.js
@@ -0,0 +1,15 @@
+// Password Generator Script
+
+// #Pseudocode
+
+// Take password criteria from the user
+// Length, character types used
+// **Output the level of security of their selected criteria?**
+
+// Randomly generate string of selected length matching user criteria
+
+// Output the string to the user's display
+
+// Allow the user to copy the string
+
+// Erase the string from memory/screen and allow the user to generate a new password
