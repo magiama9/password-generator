0eade78074fd3967539b9eb07175a21eca5bbdfb
magiama9 <58517943+magiama9@users.noreply.github.com>
2019-12-23 12:00:37 -0500

    Add function to generate character set and check if cryptography is available.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,77 @@
-// Password Generator Script
+/* Password Generator Script
 
-// #Pseudocode
+#Pseudocode
 
-// Take password criteria from the user
-// Length, character types used
-// **Output the level of security of their selected criteria?**
+Take password criteria from the user
+Length, character types used
+**Output the level of security of their selected criteria?**
 
-// Randomly generate string of selected length matching user criteria
+Randomly generate string of selected length matching user criteria
 
-// Output the string to the user's display
+Output the string to the user's display
 
-// Allow the user to copy the string
+Allow the user to copy the string
 
-// Erase the string from memory/screen and allow the user to generate a new password
+Erase the string from memory/screen and allow the user to generate a new password */
+
+// Character Sets
+var characterSets = [
+  [true, "Numbers", "0123456789"],
+  [true, "Lowercase", "abcdefghijklmnopqrstuvwxyz"],
+  [true, "Uppercase", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+  [false, "symbols", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"],
+  [false, "Space", " "]
+];
+
+// For interacting with nodes/elements of the DOM
+
+var passwordElem = document.getElementById("password");
+var numbersElem = document.getElementById("numbersCheckbox");
+var lowercaseElem = document.getElementById("lowercaseCheckbox");
+var uppercaseElem = document.getElementById("uppercaseCheckbox");
+var symbolsElem = document.getElementById("symbolsCheckbox");
+var spacesElem = document.getElementById("spacesCheckbox");
+var copyElem = document.getElementById("copy-button");
+var cryptoType = null;
+var generatedPassword = null;
+
+function chooseCrypto() {
+  if ("crypto" in window) {
+    cryptoType = crypto;
+  } else if ("msCrypto" in window) {
+    cryptoType = msCrypto;
+  } else {
+    return;
+  }
+}
+
+function createCharSet() {
+  var charSetString = "";
+  if (numbersElem.checked) {
+    console.log("Numbers is Checked");
+    charSetString += characterSets[0][2];
+    console.log(charSetString);
+  }
+  if (lowercaseElem.checked) {
+    console.log("Lowercase is Checked");
+    charSetString += characterSets[1][2];
+    console.log(charSetString);
+  }
+  if (uppercaseElem.checked) {
+    console.log("Uppercase is Checked");
+    charSetString += characterSets[2][2];
+    console.log(charSetString);
+  }
+  if (symbolsElem.checked) {
+    console.log("Symbols is Checked");
+    charSetString += characterSets[3][2];
+    console.log(charSetString);
+  }
+  if (spacesElem.checked) {
+    console.log("Spaces is Checked");
+    charSetString += characterSets[4][2];
+    console.log(charSetString);
+  }
+}
+
+createCharSet();
